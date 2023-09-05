7bc7047bdc4961711121d7c32a94298249cae81a
mershark <mersharktakyi@gmail.com>
2023-09-05 18:54:01 +0000

    Fetch missions data from api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Rockets from './components/Rockets';
-import Missions from './components/Missions';
+import Missions from './components/Missions'; // Import Missions component
 import Profile from './components/Profile';
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Rockets />} />
-        <Route path="/missions" component={Missions} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/missions" element={<Missions />} /> {/* Use 'element' prop instead of 'component' */}
+        <Route path="/profile" element={<Profile />} /> {/* Use 'element' prop instead of 'component' */}
       </Routes>
     </Router>
   );
---
+++ src/components/Missions.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missionsSlice';
+import '../redux/missions/missions.css';
+
+function Missions() {
+  const { missions, isLoading, error } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error">Error: {error}</div>;
+  }
+
+  return (
+    <div className="table-container">
+      <table className="mission-table">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th className="status">Status</th>
+            <th className="status">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td className="missionName">{mission.mission_name}</td>
+              <td className="missionDesc">{mission.description}</td>
+              <td className="status">
+                <button type="button" className="nonactiveStyle">Not a Member</button>
+              </td>
+              <td className="status">
+                <button type="button" className="reserveBtn">Join Mission</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Missions;
