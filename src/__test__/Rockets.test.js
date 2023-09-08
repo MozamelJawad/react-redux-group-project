90f200af3c3f0fbc4678976bcbae5638356cfb0a
Mozamel Jawad <mozameljawad@gmail.com>
2023-09-08 21:42:39 +0430

    add unit test for pure function

--- src/__test__/Rockets.test.js
+++ src/__test__/Rockets.test.js
@@ -1,39 +1,60 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { render } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import rocketsReducer from '../redux/rockets/rocketsSlice';
+import configureStore from 'redux-mock-store';
+import userEvent from '@testing-library/user-event';
 import Rockets from '../components/Rockets';
+import { rocketsReserved } from '../redux/rockets/rocketsSlice';
+import '@testing-library/jest-dom';
 
-describe('Render Rockets view correctly', () => {
-  test('Render rockets componect with actions correctly', () => {
-    const store = configureStore({
-      reducer: {
-        rockets: rocketsReducer,
-      },
-      initialState: {
-        rockets: {
-          rockets: [
-            {
-              id: 1, name: 'Rocket A', image: 'image1.jpg', description: 'Rocket A Description', reserved: true,
-            },
-            {
-              id: 2, name: 'Rocket B', image: 'image2.jpg', description: 'Rocket B Description', reserved: false,
-            },
-          ],
-          isLoading: false,
-          error: '',
+const mockStore = configureStore([]);
+
+test('Renders rockets componenct correctly with all button actions', () => {
+  const initialState = {
+    rockets: {
+      rockets: [
+        {
+          id: 1, name: 'Rocket A', image: 'image1.jpg', description: 'Rocket A Description', reserved: true,
         },
-      },
-    });
+        {
+          id: 2, name: 'Rocket B', image: 'image2.jpg', description: 'Rocket B Description', reserved: false,
+        },
+      ],
+      isLoading: false,
+      error: '',
+    },
+  };
 
-    const { container } = render(
-      <Provider store={store}>
-        <Router>
-          <Rockets />
-        </Router>
-      </Provider>,
-    );
-    expect(container).toMatchSnapshot();
-  });
+  const store = mockStore(initialState);
+
+  render(
+    <Provider store={store}>
+      <Router>
+        <Rockets />
+      </Router>
+    </Provider>,
+  );
+
+  const rocket1 = screen.getByText('Rocket A');
+  const description1 = screen.getByText('Rocket A Description');
+  const button1 = screen.getByText('Cancel Reservation');
+
+  const rocket2 = screen.getByText('Rocket B');
+  const description2 = screen.getByText('Rocket B Description');
+  const button2 = screen.getByText('Reserve Rockets');
+
+  expect(rocket1).toBeInTheDocument();
+  expect(description1).toBeInTheDocument();
+  expect(button1).toBeInTheDocument();
+
+  expect(rocket2).toBeInTheDocument();
+  expect(description2).toBeInTheDocument();
+  expect(button2).toBeInTheDocument();
+
+  userEvent.click(button2);
+
+  const expectedAction = rocketsReserved({ id: 2 });
+  const actions = store.getActions();
+  expect(actions).toContainEqual(expectedAction);
 });
