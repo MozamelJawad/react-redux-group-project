4523887ca2ce06c5412a731ce00ab6207069d41a
Mozamel Jawad <mozameljawad@gmail.com>
2023-09-08 21:42:54 +0430

    add unit test for pure function

--- src/__test__/Missions.test.js
+++ src/__test__/Missions.test.js
@@ -1,46 +1,55 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import missionsReducer from '../redux/missions/missionsSlice';
+import userEvent from '@testing-library/user-event';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { joinMission } from '../redux/missions/missionsSlice';
 import Missions from '../components/Missions';
+import '@testing-library/jest-dom';
 
-describe('Render Missions component correctly', () => {
-  test('Render missions component with actions correctly', async () => {
-    const store = configureStore({
-      reducer: {
-        missions: missionsReducer,
-      },
-      initialState: {
-        missions: {
-          missions: [
-            {
-              mission_id: '1',
-              mission_name: 'Mission A',
-              description: 'Mission A Description',
-              join: true,
-            },
-            {
-              mission_id: '2',
-              mission_name: 'Mission B',
-              description: 'Mission B Description',
-              join: false,
-            },
-          ],
-          isLoading: false,
-          error: '',
+const mockStore = configureStore([]);
+
+test('Render mission component with all button actions', () => {
+  const initialState = {
+    missions: {
+      missions: [
+        {
+          mission_id: 1, mission_name: 'Mission A', description: 'Description Mission A', join: true,
         },
-      },
-    });
+        {
+          mission_id: 2, mission_name: 'Mission B', description: 'Description Mission B', join: false,
+        },
+      ],
+      loading: false,
+      error: null,
+    },
+  };
 
-    const { container } = render(
-      <Provider store={store}>
-        <Router>
-          <Missions />
-        </Router>
-      </Provider>,
-    );
+  const store = mockStore(initialState);
 
-    expect(container).toMatchSnapshot();
-  });
+  render(
+    <Provider store={store}>
+      <Router>
+        <Missions />
+      </Router>
+    </Provider>,
+  );
+
+  const mission1 = screen.getByText('Mission A');
+  const description1 = screen.getByText('Description Mission A');
+  const mission2 = screen.getByText('Mission B');
+  const description2 = screen.getByText('Description Mission B');
+
+  expect(mission1).toBeInTheDocument();
+  expect(description1).toBeInTheDocument();
+  expect(mission2).toBeInTheDocument();
+  expect(description2).toBeInTheDocument();
+
+  const missionJoinedBtn = screen.getByText('Join Mission');
+  userEvent.click(missionJoinedBtn);
+
+  const expectedAction = joinMission({ mission_id: 2 });
+  const actions = store.getActions();
+  expect(actions).toContainEqual(expectedAction);
 });
