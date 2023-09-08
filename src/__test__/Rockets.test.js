c1f0513fcbba42ceea511a68f5c6069fca775ce3
Mozamel Jawad <mozameljawad@gmail.com>
2023-09-08 16:02:36 +0430

    add test for nRockets

---
+++ src/__test__/Rockets.test.js
@@ -0,0 +1,36 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import rocketsReducer from '../redux/rockets/rocketsSlice';
+import Rockets from '../components/Rockets';
+
+
+describe('Render Rockets view correctly', () => {
+        test('Render rockets componect with actions correctly', () => {
+            const store = configureStore({
+                reducer: {
+                    rockets: rocketsReducer,
+                  },
+                 initialState : {
+                    rockets: {
+                        rockets: [
+                            {id: 1, name: 'Rocket A', image:'image1.jpg', description: 'Rocket A Description', reserved: true},
+                            {id: 2, name: 'Rocket B', image:'image2.jpg', description: 'Rocket B Description', reserved: false},
+                        ],
+                        isLoading: false,
+                        error: '',
+                    },
+                 }, 
+            });
+
+            const { container } = render(
+                <Provider store={store}>
+                    <Router>
+                        <Rockets />
+                    </Router>
+                </Provider>
+            );
+            expect(container).toMatchSnapshot();
+        });
+});
